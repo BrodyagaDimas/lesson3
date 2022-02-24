d35ff4709e383420fde7c3a974045b9a71681081
user <amt24@bk.ru>
2022-02-24 19:42:53 +0300

    add fail

---
+++ 1seq.py
@@ -0,0 +1,8 @@
+n = int(input("Введите количество элементов: "))
+my_list = []
+for i in range(1, n+1):
+  х = input(f"Введите элемент № {i}: ")
+  my_list.append(x)
+
+print (my_list)
+
---
+++ home worke1.py
@@ -0,0 +1,11 @@
+# list
+append, insert, extend, remove, count, revers, clear
+# методы
+Экранированые последовательности, неформатированые строки, сложение строк, длинна строки,
+поиск подстрок, замена шаблона
+# dict
+dict.fromkeys, dict.get, dict.pop, dict.update, dict.values
+# set
+if len(set.isdisjoint, len(s), x in s, set.union, set.intersection,
+       set.difference, set.symmetric_difference:
+    
